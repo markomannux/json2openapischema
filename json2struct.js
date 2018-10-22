cfa400c65a62b82b0caf4b6e72d63102cd2b3331
Marco Mannucci <markomannux@gmail.com>
2018-10-22 11:06:40 +0200

    Parsing to itermediate struct

--- json2struct.js
+++ json2struct.js
@@ -3,30 +3,58 @@ function json2struct(jsonstring) {
     return traverse(JSON.parse(jsonstring));
 }
 
-function traverse(parsed) {
-    let struct = {};
-    for (let key in parsed) {
-        const val = parsed[key];
-        const type = typeof val;
-        struct[key] = {
-            name: key,
-            type: type,
-        };
-        
-        if (type === 'object') {
-            if (Array.isArray(val)) {
-                struct[key].type = 'array';
-                struct[key].value = val;
-            } else {
-                struct[key].value =  traverse(val, struct[key]);
-            }
-            
-        } else {
-            struct[key].value = val;
-        }
+function inferType(val) {
+    let type = typeof val;
+    if (Array.isArray(val)) {
+        type = 'array';
     }
-    
-    return struct;
+
+    return type;
+}
+
+function traverse(val, key) {
+
+    key = key || 'rootDocument'
+    const type = inferType(val)
+    const handler = handlers[type] || handlers['primitive']
+
+    return handler(key, type, val);
+}
+
+const handlers = {
+    object: handleObject,
+    array: handleArray,
+    primitive: handlePrimitive
+}
+
+function handlePrimitive(key, type, val) {
+    return {
+        name: key,
+        type: type,
+        value: val
+    };
+}
+
+function handleArray(key, type, val) {
+    const result = {
+        name: key,
+        type: type,
+        items: traverse(val[0], `${key}Item`)
+    };
+    return result;
+}
+
+function handleObject(key, type, val) {
+    const result = {
+        name: key,
+        type: type,
+        properties: {}
+    };
+    for (let k in val) {
+        const v = val[k];
+        result.properties[k] = traverse(v, k);
+    }
+    return result;
 }
 
 module.exports = json2struct;
--- json2struct.test.js
+++ json2struct.test.js
@@ -1,16 +1,25 @@
 const json2struct = require('./json2struct');
 
 test('empty string should produce empty struct', () => {
-    expect(json2struct('')).toEqual({});
+    const output = {
+        name: 'rootDocument',
+        type: 'object',
+        properties: {}
+    }
+    expect(json2struct('')).toEqual(output);
 })
 
 test('alphanumeric property should produce string type struct', () => {
     const input = '{"foo": "bar"}';
     const output = {
-        foo: {
-            name: 'foo',
-            type: 'string',
-            value: 'bar'
+        name: 'rootDocument',
+        type: 'object',
+        properties: {
+            foo: {
+                name: 'foo',
+                type: 'string',
+                value: 'bar'
+            }
         }
     }
     expect(json2struct(input)).toEqual(output);
@@ -19,10 +28,14 @@ test('alphanumeric property should produce string type struct', () => {
 test('numeric property should produce number type struct', () => {
     const input = '{"foo": 42}';
     const output = {
-        foo: {
-            name: 'foo',
-            type: 'number',
-            value: 42
+        name: 'rootDocument',
+        type: 'object',
+        properties: {
+            foo: {
+                name: 'foo',
+                type: 'number',
+                value: 42
+            }
         }
     }
     expect(json2struct(input)).toEqual(output);
@@ -31,10 +44,64 @@ test('numeric property should produce number type struct', () => {
 test('array property should produce array type struct', () => {
     const input = '{"foo": [1,2,3]}';
     const output = {
-        foo: {
-            name: 'foo',
-            type: 'array',
-            value: [1,2,3]
+        name: 'rootDocument',
+        type: 'object',
+        properties: {
+            foo: {
+                name: 'foo',
+                type: 'array',
+                items: {
+                    name: 'fooItem',
+                    type: 'number',
+                    value: 1
+                }
+            }
+        }
+    }
+    expect(json2struct(input)).toEqual(output);
+})
+
+test('empty array property', () => {
+    const input = '{"foo": []}';
+    const output = {
+        name: 'rootDocument',
+        type: 'object',
+        properties: {
+            foo: {
+                name: 'foo',
+                type: 'array',
+                items: {
+                    name: 'fooItem',
+                    type: 'undefined',
+                    value: undefined
+                }
+            }
+        }
+    }
+    expect(json2struct(input)).toEqual(output);
+})
+
+test('array of object should include object description', () => {
+    const input = '{"foo": [{"bar": 42}]}';
+    const output = {
+        name: 'rootDocument',
+        type: 'object',
+        properties: {
+            foo: {
+                name: 'foo',
+                type: 'array',
+                items: {
+                    name: 'fooItem',
+                    type: 'object',
+                    properties: {
+                        bar: {
+                            name: 'bar',
+                            type: 'number',
+                            value: 42
+                        }
+                    }
+                }
+            }
         }
     }
     expect(json2struct(input)).toEqual(output);
@@ -43,14 +110,18 @@ test('array property should produce array type struct', () => {
 test('object property should produce object type struct', () => {
     const input = '{"foo": {"bar": "foobar"}}';
     const output = {
-        foo: {
-            name: 'foo',
-            type: 'object',
-            value: {
-                bar: {
-                    name: 'bar',
-                    type: 'string',
-                    value: 'foobar'
+        name: 'rootDocument',
+        type: 'object',
+        properties: {
+            foo: {
+                name: 'foo',
+                type: 'object',
+                properties: {
+                    bar: {
+                        name: 'bar',
+                        type: 'string',
+                        value: 'foobar'
+                    }
                 }
             }
         }
